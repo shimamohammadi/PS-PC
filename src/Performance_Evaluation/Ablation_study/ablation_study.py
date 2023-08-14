1c7f907012aebfa4f9589e88d827806a69fe7bde
shimamohammadi <shima.70mdi@gmail.com>
2023-08-14 17:58:16 +0100

    ablation study added

--- src/Performance_Evaluation/Ablation_study/ablation_study.py
+++ src/Performance_Evaluation/Ablation_study/ablation_study.py
@@ -9,34 +9,58 @@ from sklearn.preprocessing import MinMaxScaler
 from src.tools import get_pcm, read_matlab_data, run_modeling_Bradley_Terry
 
 
-def frmwrk_with_clf_only(gth_prob_pcm, gth_judgments_pcm, X_test_transformed):
-    """The pcm is filled from the classifers' output. 
+def frmwrk_with_clf_only(gth_prob_pcm, gth_judgments_pcm, X_test_transformed, clf_name):
+    """This function is used to run the framework without predictor and with the clf for ONE reference
+    The pcm is filled from the classifers' output only. 
     The groundruth data is used for defer pairs, and 0.5 is used for predict pairs 
     """
-    clf_only_pcm, trial_num = clf_only(
-        gth_prob_pcm, gth_judgments_pcm, X_test_transformed)
+
+    CONDITIONS = 16
+    trial_num = 0
+    clf_only_pcm = np.zeros((CONDITIONS, CONDITIONS))
+
+    clf_only_pcm[clf_only_pcm == 0] = 0.5
+    for i in range(CONDITIONS):
+        for j in range(CONDITIONS):
+            if (i == j):
+                clf_only_pcm[i, j] = 0
+
+    # Apply classifier
+    clf_output = classify(X_test_transformed, clf_name)
+
+    # Update the prediction pcm using the classifer output
+    for row in range(CONDITIONS):
+        for col in range(row+1, CONDITIONS):
+            if (clf_output[row, col] == False):
+                clf_only_pcm[row, col] = gth_prob_pcm[row, col]
+                clf_only_pcm[col, row] = gth_prob_pcm[col, row]
+                trial_num += gth_judgments_pcm[row,
+                                               col] + gth_judgments_pcm[col, row]
 
     scores_model = infer_scores(clf_only_pcm)
 
     return scores_model, trial_num
 
 
-def frmwrk_w_predictor_only(X_test_transformed):
+def frmwrk_w_predictor_only(X_test_transformed, predictor_name):
+    """This function is used to run the framework without clf and with the predictor for ONE reference
+    """
 
-    prediction_pcm = predictor_only(X_test_transformed)
+    prediction_pcm = predictor_only(X_test_transformed, predictor_name)
     scores_model = infer_scores(prediction_pcm)
     return scores_model
 
 
-def frmwrk_with_rnd_clf_with_predictor(gth_prob_pcm, gth_judgments_pcm, X_test_transformed, num_defer_pairs):
-    """ Random classifier and predictor
+def frmwrk_with_rnd_clf_with_predictor(gth_prob_pcm, gth_judgments_pcm, X_test_transformed, num_defer_pairs, predictor_name):
+    """This function is used to run the framework with predictor and with random classifier for ONE reference 
+    Random classifier and predictor
     """
     CONDITIONS = 16
-    num_defer_pairs = 25
+    # num_defer_pairs = 25
     trial_num = 0
 
     # Apply predictor
-    prediction_pcm = predictor_only(X_test_transformed)
+    prediction_pcm = predictor_only(X_test_transformed, predictor_name)
 
     # Apply random classifeir
     marked_matrix = random_clf(num_defer_pairs)
@@ -53,24 +77,26 @@ def frmwrk_with_rnd_clf_with_predictor(gth_prob_pcm, gth_judgments_pcm, X_test_t
     return scores_model, trial_num
 
 
-def frmwrk_ps_pc(gth_prob_pcm, gth_judgments_pcm, X_test_transformed):
-    """ Framwwork = Predictor + classiifer
+def frmwrk_ps_pc(gth_prob_pcm, gth_judgments_pcm, X_test_transformed, predictor_name, clf_name):
+    """ This function is used to run the framework for ONE reference
+    Framwwork = Predictor and  classiifer
     """
 
     CONDITIONS = 16
+    trial_num = 0
 
     # Apply predictor
-    prediction_pcm = predictor_only(X_test_transformed)
+    prediction_pcm = predictor_only(X_test_transformed, predictor_name)
 
     # Apply classifer
-    clf_only_pcm, trial_num = clf_only(
-        gth_prob_pcm, gth_judgments_pcm,  X_test_transformed)
+    clf_output = classify(X_test_transformed, clf_name)
 
     # Update the prediction pcm using the classifer output
     for row in range(CONDITIONS):
-        for col in range(CONDITIONS):
-            if (clf_only_pcm[row, col] == False):
+        for col in range(row+1, CONDITIONS):
+            if (clf_output[row, col] == False):
                 prediction_pcm[row, col] = gth_prob_pcm[row, col]
+                prediction_pcm[col, row] = gth_prob_pcm[col, row]
                 trial_num += gth_judgments_pcm[row,
                                                col] + gth_judgments_pcm[col, row]
 
@@ -99,13 +125,13 @@ def read_gth(ref_name):
     return gth_prob_pcm, gth_judgments_pcm, gth_scores
 
 
-def predictor_only(X_test_transformed):
+def predictor_only(X_test_transformed, predictor_name):
     """ Apply predictor, and return a pcm
     """
     CONDITIONS = 16
     # Apply predictor
     # Predictor output. A vector not a pcm
-    predictions = predict(X_test_transformed)
+    predictions = predict(X_test_transformed, predictor_name)
     # Get pcm
     prediction_pcm = np.zeros([CONDITIONS, CONDITIONS])
 
@@ -119,40 +145,22 @@ def predictor_only(X_test_transformed):
     return prediction_pcm
 
 
-def clf_only(gth_prob_pcm, gth_judgments_pcm, X_test_transformed):
+def classify(X_test_transformed, clf_name):
     """The pcm is filled from the classifers' output. 
-    The groundruth data is used for defer pairs, and 0.5 is used for predict pairs 
     """
     CONDITIONS = 16
-    trial_num = 0
-    clf_only_pcm = np.zeros([CONDITIONS, CONDITIONS])
-    clf_output = np.zeros([CONDITIONS, CONDITIONS])
-
-    clf_only_pcm[clf_only_pcm == 0] = 0.5
-    for i in range(CONDITIONS):
-        for j in range(CONDITIONS):
-            if (i == j):
-                clf_only_pcm[i, j] = 0
+    clf_output = np.zeros((CONDITIONS, CONDITIONS))
 
     # Fill the matrix from Classifeir
     clf = pickle.load(
-        open('./src/data/Trained models/CLF/XGBOOST/clf_test_model.sav', 'rb'))
+        open('./src/data/Trained models/CLF/XGBOOST/0.98/'+clf_name, 'rb'))
     cls_results = clf.predict(X_test_transformed)
     ind_upper = np.triu_indices(CONDITIONS, 1)
     clf_output[ind_upper] = cls_results
     ind_lower = np.tril_indices(CONDITIONS, 0)
     clf_output[ind_lower] = np.nan
 
-    # Copy
-    for row in range(CONDITIONS):
-        for col in range(row+1, CONDITIONS):
-            if (clf_output[row, col] == False):
-                clf_only_pcm[row, col] = gth_prob_pcm[row, col]
-                clf_only_pcm[col, row] = gth_prob_pcm[col, row]
-                trial_num += gth_judgments_pcm[row,
-                                               col] + gth_judgments_pcm[col, row]
-
-    return clf_only_pcm, trial_num
+    return clf_output
 
 
 def infer_scores(pcm):
@@ -202,9 +210,9 @@ def normalize_features(data):
     return data_transformed
 
 
-def predict(X_test_transformed):
+def predict(X_test_transformed, predictor_name):
     predictor = pickle.load(
-        open('./src/data/Trained models/Predictor/predictor_123_model.sav', 'rb'))
+        open('./src/data/Trained models/Predictor/'+predictor_name, 'rb'))
     predictions = predictor.predict(X_test_transformed)
 
     predictions = pd.DataFrame(predictions)
@@ -224,23 +232,31 @@ def calculate_correlations(scores_gth, scores_model):
     return plcc, srocc
 
 
-def run_frmwrk_clf_only(references, ref_name):
-    """classifer output is used
+def run_frmwrk_clf_only(references, ref_names):
+    """This function is used to run the framework without predictor and with the clf for ALL references
+    classifer output is used
     predictor is not used
     """
-
+    clf_models = ['clf_123_model.sav', 'clf_456_model.sav', 'clf_789_model.sav',
+                  'clf_101112_model.sav', 'clf_131415_model.sav']
     all_ref_gth_scores = []
     all_ref_frmwrk_scores = []
     all_ref_trial_num = 0
-    for ref in references:
+    for index, ref in enumerate(references):
+
+        # Select a classifier model
+        q = index//3
+        clf_name = clf_models[q]
+
         # Read groundtruth data
-        gth_prob_pcm, gth_judgments_pcm, gth_scores = read_gth('data1')
+        gth_prob_pcm, gth_judgments_pcm, gth_scores = read_gth(
+            ref_names[index])
 
         # Read and normalize
         X_test_transformed = normalize_features(ref)
 
         frmwork_scores, trial_num = frmwrk_with_clf_only(
-            gth_prob_pcm, gth_judgments_pcm, X_test_transformed)
+            gth_prob_pcm, gth_judgments_pcm, X_test_transformed, clf_name)
 
         all_ref_gth_scores.append(gth_scores)
         all_ref_frmwrk_scores.append(frmwork_scores)
@@ -253,19 +269,28 @@ def run_frmwrk_clf_only(references, ref_name):
 
 
 def run_frmwrk_predictor_only(references, ref_names):
-    """classifer is not used
+    """This function is used to run the framework with predictor and without clf for ALL references
+    classifer is not used
     predictor is used
     """
+    predictor_models = ['predictor_123_model.sav', 'predictor_456_model.sav', 'predictor_789_model.sav',
+                        'predictor_101112_model.sav', 'predictor_131415_model.sav']
+
     all_ref_gth_scores = []
     all_ref_frmwrk_scores = []
-    for ref in references:
+    for index, ref in enumerate(references):
+        # Select a predictor model
+        q = index//3
+        predictor_name = predictor_models[q]
+
         # Read groundtruth data
-        _, _, gth_scores = read_gth('data1')
+        _, _, gth_scores = read_gth(ref_names[index])
 
         # Read and normalize
         X_test_transformed = normalize_features(ref)
 
-        frmwork_scores = frmwrk_w_predictor_only(X_test_transformed)
+        frmwork_scores = frmwrk_w_predictor_only(
+            X_test_transformed, predictor_name)
 
         all_ref_gth_scores.append(gth_scores)
         all_ref_frmwrk_scores.append(frmwork_scores)
@@ -276,23 +301,33 @@ def run_frmwrk_predictor_only(references, ref_names):
     return plcc, srocc
 
 
-def run_frmwrk_with_rnd_clf_with_predictor(references, ref_names):
-    """classifer is not used
+def run_frmwrk_with_rnd_clf_with_predictor(references, ref_names, num_defer_pairs):
+    """This function is used to run the framework with predictor and with random clf for ALL references
+    classifer is not used
     predictor is used
     """
-    num_defer_pairs = 25
+
+    predictor_models = ['predictor_123_model.sav', 'predictor_456_model.sav', 'predictor_789_model.sav',
+                        'predictor_101112_model.sav', 'predictor_131415_model.sav']
+
     all_ref_gth_scores = []
     all_ref_frmwrk_scores = []
     all_ref_trial_num = 0
-    for ref in references:
+    for index, ref in enumerate(references):
+
+        # Select a predictor model
+        q = index//3
+        predictor_name = predictor_models[q]
+
         # Read groundtruth data
-        gth_prob_pcm, gth_judgments_pcm, gth_scores = read_gth('data1')
+        gth_prob_pcm, gth_judgments_pcm, gth_scores = read_gth(
+            ref_names[index])
 
         # Read and normalize
         X_test_transformed = normalize_features(ref)
 
         frmwork_scores, trial_num = frmwrk_with_rnd_clf_with_predictor(
-            gth_prob_pcm, gth_judgments_pcm, X_test_transformed, num_defer_pairs)
+            gth_prob_pcm, gth_judgments_pcm, X_test_transformed, num_defer_pairs, predictor_name)
 
         all_ref_gth_scores.append(gth_scores)
         all_ref_frmwrk_scores.append(frmwork_scores)
@@ -305,21 +340,36 @@ def run_frmwrk_with_rnd_clf_with_predictor(references, ref_names):
 
 
 def run_frmwrk_ps_pc(references, ref_names):
-    """classifer is not used
+    """This function is used to run the framework for ALL references
+    classifer is not used
     predictor is used
     """
+
+    predictor_models = ['predictor_123_model.sav', 'predictor_456_model.sav', 'predictor_789_model.sav',
+                        'predictor_101112_model.sav', 'predictor_131415_model.sav']
+
+    clf_models = ['clf_123_model.sav', 'clf_456_model.sav', 'clf_789_model.sav',
+                  'clf_101112_model.sav', 'clf_131415_model.sav']
+
     all_ref_gth_scores = []
     all_ref_frmwrk_scores = []
     all_ref_trial_num = 0
-    for ref in references:
+    for index, ref in enumerate(references):
+
+        # Select a classifier and predictor model
+        q = index//3
+        clf_name = clf_models[q]
+        predictor_name = predictor_models[q]
+
         # Read groundtruth data
-        gth_prob_pcm, gth_judgments_pcm, gth_scores = read_gth('data1')
+        gth_prob_pcm, gth_judgments_pcm, gth_scores = read_gth(
+            ref_names[index])
 
         # Read and normalize
         X_test_transformed = normalize_features(ref)
 
         frmwork_scores, trial_num = frmwrk_ps_pc(
-            gth_prob_pcm, gth_judgments_pcm, X_test_transformed)
+            gth_prob_pcm, gth_judgments_pcm, X_test_transformed, predictor_name, clf_name)
 
         all_ref_gth_scores.append(gth_scores)
         all_ref_frmwrk_scores.append(frmwork_scores)
@@ -327,6 +377,7 @@ def run_frmwrk_ps_pc(references, ref_names):
 
     plcc, srocc = calculate_correlations(
         all_ref_gth_scores, all_ref_frmwrk_scores)
+    # print(all_ref_frmwrk_scores)
 
     return plcc, srocc, all_ref_trial_num
 
@@ -342,22 +393,49 @@ def main():
         './src/data/IQA features/reference_4_features.csv', header=0, sep=",")
     df_data5 = pd.read_csv(
         './src/data/IQA features/reference_5_features.csv', header=0, sep=",")
+    df_data6 = pd.read_csv(
+        './src/data/IQA features/reference_6_features.csv', header=0, sep=",")
+    df_data7 = pd.read_csv(
+        './src/data/IQA features/reference_7_features.csv', header=0, sep=",")
+    df_data8 = pd.read_csv(
+        './src/data/IQA features/reference_8_features.csv', header=0, sep=",")
+    df_data9 = pd.read_csv(
+        './src/data/IQA features/reference_9_features.csv', header=0, sep=",")
+    df_data10 = pd.read_csv(
+        './src/data/IQA features/reference_10_features.csv', header=0, sep=",")
+    df_data11 = pd.read_csv(
+        './src/data/IQA features/reference_11_features.csv', header=0, sep=",")
+    df_data12 = pd.read_csv(
+        './src/data/IQA features/reference_12_features.csv', header=0, sep=",")
+    df_data13 = pd.read_csv(
+        './src/data/IQA features/reference_13_features.csv', header=0, sep=",")
+    df_data14 = pd.read_csv(
+        './src/data/IQA features/reference_14_features.csv', header=0, sep=",")
+    df_data15 = pd.read_csv(
+        './src/data/IQA features/reference_15_features.csv', header=0, sep=",")
 
-    references = [df_data1, df_data2, df_data3, df_data4, df_data5]
-    ref_names = ['data1', 'data2', 'data3', 'data4', 'data5']
+    references = [df_data1, df_data2, df_data3, df_data4, df_data5, df_data6, df_data7,
+                  df_data8, df_data9, df_data10, df_data11, df_data12, df_data13, df_data14, df_data15]
+    ref_names = ['data1', 'data2', 'data3', 'data4', 'data5', 'data6', 'data7',
+                 'data8', 'data9', 'data10', 'data11', 'data12', 'data13', 'data14', 'data15']
 
+    # Classifier only
     plcc, srocc, trials = run_frmwrk_clf_only(references, ref_names)
     print(f"plcc is: {plcc}, srocc is: {srocc}, and trial is{trials}")
 
+    # Predictor only
     plcc, srocc = run_frmwrk_predictor_only(references, ref_names)
     print(f"plcc is: {plcc}, srocc is: {srocc}")
 
+    # RndClass+predict
     plcc_vector = []
     srocc_vector = []
     trial_vector = []
-    for repeat in range(1):
+    # try this numbers => 10, 13, 17, 20, 25
+    num_defer_pairs = 25
+    for repeat in range(25):
         plcc, srocc, trials = run_frmwrk_with_rnd_clf_with_predictor(
-            references, ref_names)
+            references, ref_names, num_defer_pairs)
         plcc_vector.append(plcc)
         srocc_vector.append(srocc)
         trial_vector.append(trials)
@@ -365,6 +443,7 @@ def main():
     print(
         f" Average plcc is: {sum(plcc_vector)/len(plcc_vector)}, average srocc is: {sum(srocc_vector)/len(srocc_vector)}, and average trail is:{sum(trial_vector)/len(trial_vector)}")
 
+    # PS-PC
     plcc, srocc, trials = run_frmwrk_ps_pc(references, ref_names)
     print(f"plcc is: {plcc}, srocc is: {srocc}, and trails is {trials}")
 
--- src/Training/Groundtruth_data_collection/labeling.py
+++ src/Training/Groundtruth_data_collection/labeling.py
@@ -11,34 +11,69 @@ def write_to_csv(labling_res, writer, ref_name):
     for row in range(len(labling_res)):
         for col in range(row+1, len(labling_res)):
             writer.writerow(
-                ["ref_"+ref_name+"_Img"+str(row+1), "ref_1_Img"+str(col+1), labling_res[row, col]])
+                ["ref_"+ref_name+"_Img"+str(row+1), "ref_"+ref_name+"_Img"+str(col+1), labling_res[row, col]])
 
 
 def run_kld_labeling():
-    df_data1 = pd.read_csv(
-        './src/data/IQA features/reference_1_features.csv', header=0, sep=",")
-    df_data2 = pd.read_csv(
-        './src/data/IQA features/reference_2_features.csv', header=0, sep=",")
-    df_data3 = pd.read_csv(
-        './src/data/IQA features/reference_3_features.csv', header=0, sep=",")
+    # df_data1 = pd.read_csv(
+    #     './src/data/IQA features/reference_1_features.csv', header=0, sep=",")
+    # df_data2 = pd.read_csv(
+    #     './src/data/IQA features/reference_2_features.csv', header=0, sep=",")
+    # df_data3 = pd.read_csv(
+    #     './src/data/IQA features/reference_3_features.csv', header=0, sep=",")
     df_data4 = pd.read_csv(
         './src/data/IQA features/reference_4_features.csv', header=0, sep=",")
     df_data5 = pd.read_csv(
         './src/data/IQA features/reference_5_features.csv', header=0, sep=",")
-    REF = 5
+    df_data6 = pd.read_csv(
+        './src/data/IQA features/reference_6_features.csv', header=0, sep=",")
+    df_data7 = pd.read_csv(
+        './src/data/IQA features/reference_7_features.csv', header=0, sep=",")
+    df_data8 = pd.read_csv(
+        './src/data/IQA features/reference_8_features.csv', header=0, sep=",")
+    df_data9 = pd.read_csv(
+        './src/data/IQA features/reference_9_features.csv', header=0, sep=",")
+    df_data10 = pd.read_csv(
+        './src/data/IQA features/reference_10_features.csv', header=0, sep=",")
+    df_data11 = pd.read_csv(
+        './src/data/IQA features/reference_11_features.csv', header=0, sep=",")
+    df_data12 = pd.read_csv(
+        './src/data/IQA features/reference_12_features.csv', header=0, sep=",")
+    df_data13 = pd.read_csv(
+        './src/data/IQA features/reference_13_features.csv', header=0, sep=",")
+    df_data14 = pd.read_csv(
+        './src/data/IQA features/reference_14_features.csv', header=0, sep=",")
+    df_data15 = pd.read_csv(
+        './src/data/IQA features/reference_15_features.csv', header=0, sep=",")
+
+    REF = 12
     CONDITIONS = 16
     plcc_kld = []
     srocc_kld = []
-    ref_1 = KLDClass(df_data1, 'data1')
-    ref_2 = KLDClass(df_data2, 'data2')
-    ref_3 = KLDClass(df_data3, 'data3')
+    # ref_1 = KLDClass(df_data1, 'data1')
+    # ref_2 = KLDClass(df_data2, 'data2')
+    # ref_3 = KLDClass(df_data3, 'data3')
     ref_4 = KLDClass(df_data4, 'data4')
     ref_5 = KLDClass(df_data5, 'data5')
-    gth_probs_arr = np.array([ref_1.gth_prob, ref_2.gth_prob,
-                             ref_3.gth_prob, ref_4.gth_prob, ref_5.gth_prob])
+    ref_6 = KLDClass(df_data5, 'data6')
+    ref_7 = KLDClass(df_data5, 'data7')
+    ref_8 = KLDClass(df_data5, 'data8')
+    ref_9 = KLDClass(df_data5, 'data9')
+    ref_10 = KLDClass(df_data5, 'data10')
+    ref_11 = KLDClass(df_data5, 'data11')
+    ref_12 = KLDClass(df_data5, 'data12')
+    ref_13 = KLDClass(df_data5, 'data13')
+    ref_14 = KLDClass(df_data5, 'data14')
+    ref_15 = KLDClass(df_data5, 'data15')
+    gth_probs_arr = np.array([
+        ref_4.gth_prob, ref_5.gth_prob, ref_6.gth_prob,
+        ref_7.gth_prob, ref_8.gth_prob, ref_9.gth_prob,
+        ref_10.gth_prob, ref_11.gth_prob, ref_12.gth_prob,
+        ref_13.gth_prob, ref_14.gth_prob, ref_15.gth_prob])
 
     # available reference objects
-    reference_obj = [ref_1, ref_2, ref_3, ref_4, ref_5]
+    reference_obj = [ref_4, ref_5, ref_6, ref_7,
+                     ref_8, ref_9, ref_10, ref_11, ref_12, ref_13, ref_14, ref_15]
     iteration = 0
 
     # Taken out three reference as test set, (n(n-1)/2)*12 = 1400 pairs remains in the dataset
@@ -86,8 +121,11 @@ def run_kld_labeling():
             reference_obj[selected_ref_idx].current_pcm)
 
         # An array of all the scores for all the references
-        current_probs_arr = np.array([ref_1.current_prob, ref_2.current_prob,
-                                     ref_3.current_prob, ref_4.current_prob, ref_5.current_prob])
+        current_probs_arr = np.array([
+            ref_4.current_prob, ref_5.current_prob, ref_6.current_prob,
+            ref_7.current_prob, ref_8.current_prob, ref_9.current_prob,
+            ref_10.current_prob, ref_11.current_prob, ref_12.current_prob,
+            ref_13.current_prob, ref_14.current_prob, ref_15.current_prob])
 
         # Calculate PLCC between all the current_pcm scores and the groundtruth scores
         plcc, _ = pearsonr(current_probs_arr, gth_probs_arr)
@@ -115,11 +153,21 @@ def main():
         writer = csv.writer(file)
         writer.writerow(['Pair_a', 'Pair_b', 'Defer'])
 
-        write_to_csv(reference_obj[0].lbl_pairs, writer, "1")
-        write_to_csv(reference_obj[1].lbl_pairs, writer, "2")
-        write_to_csv(reference_obj[2].lbl_pairs, writer, "3")
-        write_to_csv(reference_obj[3].lbl_pairs, writer, "4")
-        write_to_csv(reference_obj[4].lbl_pairs, writer, "5")
+        write_to_csv(reference_obj[0].lbl_pairs, writer, "4")
+        write_to_csv(reference_obj[1].lbl_pairs, writer, "5")
+        write_to_csv(reference_obj[2].lbl_pairs, writer, "6")
+        write_to_csv(reference_obj[3].lbl_pairs, writer, "7")
+        write_to_csv(reference_obj[4].lbl_pairs, writer, "8")
+        write_to_csv(reference_obj[5].lbl_pairs, writer, "9")
+        write_to_csv(reference_obj[6].lbl_pairs, writer, "10")
+        write_to_csv(reference_obj[7].lbl_pairs, writer, "11")
+        write_to_csv(reference_obj[8].lbl_pairs, writer, "12")
+        write_to_csv(reference_obj[9].lbl_pairs, writer, "13")
+        write_to_csv(reference_obj[10].lbl_pairs, writer, "14")
+        write_to_csv(reference_obj[11].lbl_pairs, writer, "15")
+        # write_to_csv(reference_obj[12].lbl_pairs, writer, "16")
+        # write_to_csv(reference_obj[13].lbl_pairs, writer, "14")
+        # write_to_csv(reference_obj[14].lbl_pairs, writer, "15")
 
 
 if __name__ == '__main__':
